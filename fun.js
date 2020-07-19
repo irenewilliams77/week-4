3a5c5cb6a102bbeb762deb0eb2fc52ed1c58092d
Irene Williams <europeandistribution@ymail.com>
2020-07-19 13:45:59 -0400

    finished hw4 number3

---
+++ fun.js
@@ -0,0 +1,43 @@
+var name = "Irene";
+var age = 50;
+console.log(name +" "+ age)
+
+
+
+function newSum(one,two){
+    var multiply = (one*two);
+    return multiply;
+    }
+    console.log(newSum(7,9));
+    
+function oldSum(why,me){
+    var added = (why*me);
+    return added;
+    }
+    console.log(oldSum(5,10));
+  
+function counter(beginning, end) {
+     for (beginning; beginning <=end; beginning++) {
+        console.log(beginning); 
+       }
+    }
+        
+     counter(5,9);
+
+
+
+var age = 50;
+function adding(){
+   
+}
+console.log(age);
+
+function calling(){
+    console.log("this is my second time around IIFE");
+}
+calling();
+
+
+
+
+
