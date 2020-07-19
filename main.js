d7c4e8c912f9a566aca19af8bdd56469dbb77349
Irene Williams <europeandistribution@ymail.com>
2020-07-19 10:55:31 -0400

    added hw4 #2

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
-var string = "5";
-var number = 3;
-console.log(string*number);
+//var string = "5";
+//var number = 3;
+//console.log(string*number);
 
-console.log(true+2);
+//console.log(true+2);
+
+var counter = 2;
+console.log (counter + 5);
+console.log ((counter + 5)*2);
+console.log(((counter + 5)*2)%3);
+
+var number = 4;
+if (number % 2 == 0){
+    console.log("even");
+}else{
+    console.log("odd");
+}
+console.log(number)
+
+
+
+var none = false;
+var IGotMoney = true;
+var IGotNoMoney = false;
+var money = 30;
+
+    if (IGotMoney && money){
+        console.log(" Go to  the store");
+    } else{
+         if(IGotNoMoney || money){
+         console.log("Go home");
+        
+    } else { 
+        console.log("Find a friend and borrow money");
+    }
+    }
+console.log(IGotMoney);
+console.log(IGotNoMoney);
+console.log(none);
+
+
+
+
+
+
+
+
+
