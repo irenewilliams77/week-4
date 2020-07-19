803fd2028743ca39bfad3b4656d63ce1215161f0
Irene Williams <europeandistribution@ymail.com>
2020-07-19 18:47:14 -0400

    created tunes files

--- tunes.js
+++ tunes.js
@@ -1,4 +1,27 @@
-function mybtn (){
-    var submit = clickOn
+function print() {
+    var input = 
+    document.getElementById("userInput").value;
+    document.getElementById("output").innerHTML  = input;
+  }
+  function print (){
+    var print = document.getElementById("userInput").value;
+       if (print === null || print === ""){
+       prompt("Please request song");
+       }else{
+           prompt("Thank you!")
+       }
+   }
+   function print(){
+    var print = document.getElementById("submit");
+    userInput = input.value;
+    document.getElementById("output").innerHTML = input;
 }
-var array = ""
+//var requestSong = window.prompt ("Please request a song");
+//Responds();
+
+//function Responds() {
+   // document.write("Thank you for requesting the song" + " " + requestSong);
+//}
+
+
+
