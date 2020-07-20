800a0a4f1f79c8be16882bdadcd1845c2ae65677
Irene Williams <europeandistribution@ymail.com>
2020-07-19 22:07:22 -0400

    modified button

--- tunes.js
+++ tunes.js
@@ -3,19 +3,30 @@ function print() {
     document.getElementById("userInput").value;
     document.getElementById("output").innerHTML  = input;
   }
-  function print (){
-    var print = document.getElementById("userInput").value;
-       if (print === null || print === ""){
-       prompt("Please request song");
-       }else{
-           prompt("Thank you!")
-       }
+ // function print (){
+  //  var print = document.getElementById("userInput").value;
+  //     if (print === null || print === ""){
+  //     prompt("Please request song");
+  //     }else{
+ //          prompt("Thank you!")
+ //      }
+ //  }
+   var userInput = document.getElementById("userInput").value;
+   songRequest = document.getElementById("output").innerHTML;
+   button = document.getElementById(submit);
+
+   button.onclick = function(){
+       enterSong(userInput);
    }
-   function print(){
-    var print = document.getElementById("submit");
-    userInput = input.value;
-    document.getElementById("output").innerHTML = input;
-}
+   var newSong = function(userSong){
+       var output = ("userInput");
+       return songRequest.innerHTML = output;
+   }
+  // function print(){
+   // var print = document.getElementById("submit");
+    //userInput = input.value;
+    //document.getElementById("output").innerHTML = input;
+//}
 //var requestSong = window.prompt ("Please request a song");
 //Responds();
 
